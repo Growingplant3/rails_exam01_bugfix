37dd8a1753292c6da762b0169a88840b487ce1b2
Yoshinori Shimoda <ys@platinum.local>
2021-02-10 15:49:23 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :blog do
+  # エラーの原因: blogs_pathでrouting errorを起こす
+  # 修正の意図: resources :blogの記述になっていたのでコードの書き方が違ったものを直す必要があった
+  resources :blogs do
     resources :comments, only: [:create, :destroy]
   end
 end
